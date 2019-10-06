3982f7d81f2c9f8123335974c6d869da2348ddae
Jordan Heffernan <jheff2@gmail.com>
2019-10-05 19:04:30 -0700

    close button for help info

--- src/components/HowToPlay.jsx
+++ src/components/HowToPlay.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleHelp } from '../store/actions';
 
 import '../styles/HowToPlay.css';
 
-const HowToPlay = props => {
-  return (
-    <section id="howTo">
-      <h2>What do I do?</h2>
-      <p>This is a more advanced version of Rock Paper Scissors</p>
-      <ul>
-        <li>Pick one of the options below</li>
-        <li>When you hit submit, I will randomly select one of the other options</li>
-        <li>Below you will get feedback on who won each round</li>
-        <li>You can select how many rounds we are playing "the best of"</li>
-        <li>Whoever wins the necessary rounds first, wins the game!</li>
-        <li>You can press "New Game" at any time to reset</li>
-      </ul>
-    </section>
-  );
+export class HowToPlay extends React.Component {
+  onToggle() {
+    this.props.dispatch(toggleHelp());
+  }
+  render() {
+    return (
+      <section id="howTo">
+        <h2>What do I do?</h2>
+        <p>This is a more advanced version of Rock Paper Scissors</p>
+        <ul>
+          <li>Pick one of the options below</li>
+          <li>When you hit submit, I will randomly select one of the other options</li>
+          <li>Below you will get feedback on who won each round</li>
+          <li>You can select how many rounds we are playing "the best of"</li>
+          <li>Whoever wins the necessary rounds first, wins the game!</li>
+          <li>You can press "New Game" at any time to reset</li>
+        </ul>
+        <button
+          type="button"
+          name="close"
+          id="close"
+          className="navButton"
+          onClick={() => this.onToggle()}
+        >
+          Go Back to Game
+        </button>
+      </section>
+    );
+  }
 }
-
-export default HowToPlay;
+export default connect()(HowToPlay);
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 
 import { restartGame, toggleHelp } from '../store/actions';
 
-
 import '../styles/NavBar.css';
 
 
